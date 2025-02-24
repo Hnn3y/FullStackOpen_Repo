20b082ff563c4f30367c6fb5a38fcb3f10893685
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-02-24 12:56:35 +0100

    REACT

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -4,24 +4,29 @@ const App = () => {
   const b = 40
   console.log(now, a+b)
 
+  const name = 'Peter'
+  const age = 10
 
 
 
 
-  console.log("Hello From component")
+
   return (
     <div>
       <p>Hello world</p>
-      <Hello name = "Henneiy" />
-      <Hello name = "Simon"/>
-      <Hello />
+      <h1>Greetings</h1>
+      <Hello name = "Henneiy" age={26+5}/>
+      <Hello name = "Simon" age={20+10}/>
+      <Hello name={name} age={age}/>
     </div>
   )
 }
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>
+       <h1> Hello {props.name}, you are {props.age} years old</h1>
+        </p>
     </div>
   )
 }
