0d39352ad896a82d9421276f862f75f114b664fc
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-02-24 12:35:53 +0100

    REACT

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,8 +1,27 @@
 const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 40
+  console.log(now, a+b)
+
+
+
+
+
   console.log("Hello From component")
   return (
     <div>
       <p>Hello world</p>
+      <Hello name = "Henneiy" />
+      <Hello name = "Simon"/>
+      <Hello />
+    </div>
+  )
+}
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
     </div>
   )
 }
